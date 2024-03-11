d1320305ce30f4cc099227f6c931d8f7246d14a3
andria12344 <163038391+andria12344@users.noreply.github.com>
2024-03-11 23:27:44 +0400

    Create day01.py

---
+++ day01/day01.py
@@ -0,0 +1,8 @@
+name="andria"
+surname="abuladze"
+age="13 wlis"
+fro="axalcixe"
+text="programistobas vswavlob da minda viko programisti"
+print(name + " " + surname)
+print(age + " " + fro)
+print(text)
